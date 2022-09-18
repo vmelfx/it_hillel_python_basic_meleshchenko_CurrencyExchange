f2c3cf9dbbc890b3a858cee77bb0944affcae7f6
vmelfx <vladimir.meleshchenko@outllok.com>
2022-09-19 00:18:08 +0300

    Commit #2 18.09.2022.  - Added error handling in main (will complete later)  - Completed menu

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ class Cashier:
         currency_rate = self.get_currency_rate()
         if currency == 'usd':
             current_usd_balance = self.get_usd_balance()
-            print("RATE: ", currency_rate, "AVAILABLE: ", current_usd_balance)
+            print("\n\nRATE: ", currency_rate, "AVAILABLE: ", current_usd_balance, "\n\n")
         elif currency == 'uah':
             current_uah_balance = self.get_uah_balance()
-            print("RATE: ", currency_rate, "AVAILABLE: ", current_uah_balance)
+            print("\n\nRATE: ", currency_rate, "AVAILABLE: ", current_uah_balance, "\n\n")
         else:
             print(f"Invalid currency{currency}")
 
@@ -71,52 +71,61 @@ class Cashier:
 
 
 def main():
-    print("Hi. Before use main functionality, you need to specify your USD and UAH balance")
-    usd_count = int(input("Please, enter your USD balance (only digits accepted): "))
-    uah_count = int(input("Please, enter your UAH balance (only digits accepted): "))
-    cashier = Cashier(usd_count=usd_count, uah_count=uah_count)
-    while True:
-        print("Please choose one of the following action (just type the action name:",
-              "RATE (Access to the currency rate and balance operations)",
-              "EXCHANGE (Access to exchanging USD-UAH or UAH-USD)",
-              "EXIT (Exit from the program)", sep='\n')
-        action = input("Action: ").lower()
-        match action:
-            case "rate":
-                print("Please, choose one of the following actions:",
-                      "RATE USD", "RATE UAH", "EXIT - will return you to previous menu", sep='\n')
-                rate_action = input("Action: ").lower()
-                match rate_action:
-                    case "rate usd":
-                        cashier.rate_and_cash_balance(currency='usd')
-                    case "rate uah":
-                        cashier.rate_and_cash_balance(currency='uah')
-                    case "exit":
-                        print("Exiting to main menu...")
-                    case _:
-                        print("Invalid input! You will be returned to main menu.")
-            case "exchange":
-                while True:
-                    print("Please, choose one of the following actions:",
-                          "EXCHANGE USD", "EXCHANGE UAH", "EXIT - will return you to previous menu", sep='\n')
-                    exchange_action = input("Action: ").lower()
-                    match exchange_action:
-                        case "exchange usd":
-                            amount_usd = int(input("Please, enter the amount of usd you want to exchange"
-                                                   " (only digits accepted): "))
-                            cashier.currency_exchange(amount_usd, currency_to_exchange='usd')
-                        case "exchange uah":
-                            amount_uah = int(input("Please, enter the amount of uah you want to exchange"
-                                                   " (only digits accepted): "))
-                            cashier.currency_exchange(amount_uah, currency_to_exchange='uah')
-                        case "exit":
-                            print("Exiting to main menu...")
-                        case _:
-                            print("Invalid input! Try again.")
-            case "exit":
-                raise SystemExit(0)
-            case _:
-                print()
+    try:
+        print("Hi. Before use main functionality, you need to specify your USD and UAH balance")
+        usd_count = int(input("Please, enter your USD balance (only digits accepted): "))
+        uah_count = int(input("Please, enter your UAH balance (only digits accepted): "))
+        cashier = Cashier(usd_count=usd_count, uah_count=uah_count)
+        while True:
+            print("Please choose one of the following action (just type the action name:",
+                  "--RATE-- (Access to the currency rate and balance operations)",
+                  "--EXCHANGE-- (Access to exchanging USD-UAH or UAH-USD)",
+                  "--EXIT-- (Exit from the program)", sep='\n')
+            action = input("Action: ").lower()
+            match action:
+                case "rate":
+                    while True:
+                        print("Please, choose one of the following actions:",
+                              "--RATE USD-- (will show you current exchange rate and availability(USD))",
+                              "--RATE UAH-- (will show you current exchange rate and availability(UAH))",
+                              "--EXIT--  will return you to previous menu", sep='\n')
+                        rate_action = input("Action: ").lower()
+                        match rate_action:
+                            case "rate usd":
+                                cashier.rate_and_cash_balance(currency='usd')
+                            case "rate uah":
+                                cashier.rate_and_cash_balance(currency='uah')
+                            case "exit":
+                                print("Exiting to main menu...")
+                                break
+                            case _:
+                                print("\n\nInvalid input! Try again.\n\n")
+                case "exchange":
+                    while True:
+                        print("Please, choose one of the following actions:",
+                              "--EXCHANGE USD--", "--EXCHANGE UAH--", "--EXIT-- (will return you to previous menu)",
+                              sep='\n')
+                        exchange_action = input("Action: ").lower()
+                        match exchange_action:
+                            case "exchange usd":
+                                amount_usd = int(input("Please, enter the amount of usd you want to exchange"
+                                                       " (only digits accepted): "))
+                                cashier.currency_exchange(amount_usd, currency_to_exchange='usd')
+                            case "exchange uah":
+                                amount_uah = int(input("Please, enter the amount of uah you want to exchange"
+                                                       " (only digits accepted): "))
+                                cashier.currency_exchange(amount_uah, currency_to_exchange='uah')
+                            case "exit":
+                                print("Exiting to main menu...")
+                                break
+                            case _:
+                                print("\n\nInvalid input! Try again.\n\n")
+                case "exit":
+                    raise SystemExit(0)
+                case _:
+                    print("\n\nInvalid input! Try again.\n\n")
+    except KeyboardInterrupt:
+        print("\nProgram exited via keyboard interrupt. Good bye!")
 
 
 if __name__ == '__main__':
